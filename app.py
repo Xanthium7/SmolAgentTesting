b99316029f998716f5f8197fa550145d6651bcd2
Xanthium7 <akshhaykmurali@gmail.com>
2025-01-08 23:19:25 +0530

    Refactor Hugging Face model initialization to use login function

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from smolagents import CodeAgent, HfApiModel, DuckDuckGoSearchTool
 import os
 from dotenv import load_dotenv
+from huggingface_hub import login
+
 
 load_dotenv()
 
+login(token=os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+
 model_id = "meta-llama/Llama-3.3-70B-Instruct"
-model = HfApiModel(model_id=model_id, token=os.getenv(
-    "HUGGINGFACEHUB_API_TOKEN"))
+model = HfApiModel(model_id=model_id)
 print(os.getenv("HUGGINGFACEHUB_API_TOKEN"))
 agent = CodeAgent(tools=[DuckDuckGoSearchTool],
                   model=model)
